ee9c9944eaed4c491a4dfe7bb13d66c4a0688116
Juliana Karoline <juliana.karoline@catijr.com.br>
2019-10-17 21:01:04 -0300

    Test parser

---
+++ test_parser.py
@@ -0,0 +1,14 @@
+import pytest
+
+from questions_parser import parser_list
+
+
+@pytest.fixture
+def questions():
+    return [" a", "a ", " a "]
+
+
+def test_parser_list(questions):
+    assert parser_list(questions) == ["a", "a", "a"]
+
+
