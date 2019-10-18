6ce5e76cd3bb1335d9ca25d1105f41d260353b55
Juliana Karoline <julianaklulo@gmail.com>
2019-10-18 17:07:58 -0300

    Refactor to exit script when no mode is specified

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import random
+import sys
 
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
@@ -18,16 +19,25 @@ TOKEN = os.getenv("TOKEN")
 
 mode = os.getenv("MODE")
 if mode == "dev":
+
     def run(updater):
         updater.start_polling()
+
+
 elif mode == "prod":
+
     def run(updater):
         PORT = int(os.environ.get("PORT", "8443"))
         HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
-        updater.start_webhook(listen="0.0.0.0",
-                            port=PORT,
-                            url_path=TOKEN)
-        updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
+        updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
+        updater.bot.set_webhook(
+            "https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN)
+        )
+
+
+else:
+    logger.error("No MODE specified")
+    sys.exit(1)
 
 
 def add_handler(dispatcher):
