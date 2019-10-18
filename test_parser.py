3a936e75f752626548c9ecca5d50d460e18e12ed
Juliana Karoline <julianaklulo@gmail.com>
2019-10-17 23:35:41 -0300

    Fix params for test

--- test_parser.py
+++ test_parser.py
@@ -3,10 +3,9 @@ import pytest
 from questions_parser import parser_list
 
 
-@pytest.fixture
-def questions():
-    return [" a", "a ", " a "]
-
-
-def test_parser_list(questions):
-    assert parser_list(questions) == ["a", "a", "a"]
+@pytest.mark.parametrize(
+    "questions, expected_questions",
+    [(["\n\n", " \na \n\n", "   "], ["a"]), ([" a", "a ", "a"], ["a", "a", "a"])],
+)
+def test_parser_list(questions, expected_questions):
+    assert parser_list(questions) == expected_questions
